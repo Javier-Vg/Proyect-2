71a6f701d0404ef7f5b68f2cbf5a451654c6dffd
Javier Vargas Guzman <Javier-Vg>
2024-05-09 03:33:02 -0600

    registro5

--- script.js
+++ script.js
@@ -21,23 +21,29 @@ function RegistrarUsuario(){
         Direccion : Direccion.value
     };
 
+    registros = JSON.parse(localStorage.getItem("registros"));
 
-   
-    let registro = JSON.parse(localStorage.getItem("registros"));
-    
-    //Hacer un for para recorrer todos los elementos del loclStrage!!!!!!!!!!!!!!!!!!!!
-    if (Correo.value != registro[0].Nombre) {
+    if (registros) {
+
+        //El for verifica si ya existe un correo con ese nombre:
+        for (let elemento in registros) {
+            if (Correo.value == registros[elemento].Correo) {
+                return(alert("El correo ya existe!, acceda a su cuenta desde el login."));
+            }
+        }
+
+        //Si el correo no se repite, agrega el perfil.
         registros = JSON.parse(localStorage.getItem("registros")) || [];
-
         registros.push(persona);
-
         localStorage.setItem("registros", JSON.stringify(registros));
+        alert("El correo se agrego exitosamente");
+
     }else{
-        alert("El correo ya existe!");
-        
-    }
-    //console.log(JSON.parse(localStorage.getItem("registros")));
-    
+        registros = JSON.parse(localStorage.getItem("registros")) || [];
+        registros.push(persona);
+        localStorage.setItem("registros", JSON.stringify(registros)); 
+        alert("¡Fuiste el primero en registrarte!");
+    }   
 }
 
 
@@ -50,32 +56,91 @@ function RegistrarUsuario(){
 
 function Login() {
     //El correo es la llave
-
+    let sesionIniciada = JSON.parse(localStorage.getItem("sesionAbierta"));
     let CorreoVerific = document.getElementById("login1").value;
     let ContraVerific = document.getElementById("login2").value;
 
-    if ( localStorage.getItem(CorreoVerific)) {
+    let TotalRegistros = JSON.parse(localStorage.getItem("registros"));
+    if (TotalRegistros) {
+        if (sesionIniciada) {
+            alert("Debe de cerrar sesion para ingresar a un nuevo perfil !");
+        } else {
+            let datos = 3;
+            for (let elemento in TotalRegistros) {
+                if (CorreoVerific == TotalRegistros[elemento].Correo) {
 
-        let llave = JSON.parse(localStorage.getItem(CorreoVerific));
+                    datos = 1;
+
+                    if (ContraVerific == TotalRegistros[elemento].Contraseña) {
+                        
+                        datos = 2;
+                        //Se encuentra el correo y crea una nueva llave en el localeStrg.
+                        alert("Redirigiendo a su cuenta...");
+                        cont_correo.innerHTML = TotalRegistros[elemento].Correo;
+                        cont_nom.innerHTML =TotalRegistros[elemento].Nombre;
+                        cont_direccion.innerHTML =TotalRegistros[elemento].Direccion;
+                        localStorage.setItem("sesionAbierta", JSON.stringify(TotalRegistros[elemento])); 
+                    }
+                }
+            }
+            //Verifica si el correo o la contraseña existian en el localStorage.
+            console.log(datos)
+            if (datos == 1) {
+                alert("Contraseña inválida para el correo proporcionado.");
+            }else if ( datos == 2){
+                dato = 2;
+            }else if(datos == 3){
+                alert("No existe ese correo. Registrese...");
+            }
+        }
+    }else{ 
+        alert("No existen correos registrados.");
+    }   
+    
+}
+
+
+
+
+function desLogearse() {
+    let usuario = localStorage.getItem("sesionAbierta");
+    if (usuario) {
+        alert("Cerrando sesion...");
+        localStorage.removeItem("sesionAbierta");
+        cont_correo.innerHTML = "";
+        cont_nom.innerHTML = "";
+        cont_direccion.innerHTML = "";
         
-         if (ContraVerific.trim() === llave.Contraseña.trim()) {
-              alert("Haz cambiado de perfil a "+llave.Nombre);
-              cont_nom.innerHTML = llave.Nombre;
-              cont_correo.innerHTML = llave.Correo;
-              cont_direccion.innerHTML = llave.Direccion;
-              
-
-
-              //console.log(llave.Correo);
-              //console.log(llave.Contraseña);
-              //console.log(llave.Nombre);
-           
-         }else{
-             alert("La contraseña es incorrecta");
-         }
     }else{
-        alert("El correo no existe");
+       alert("Aun no haz entrado a una cuenta.")
     }
+    
+}
+
+let titulo = document.getElementById("porta1");
+let descripcion = document.getElementById("porta2");
+let año = document.getElementById("porta3");
+
+let usuario = (localStorage.getItem("sesionAbierta"));
+
+let proyectList = [];
+proyectList = JSON.parse(localStorage.getItem("Proyectos"));
+
+//Corregir esto------------------------------------------------------------------------
+function portafolios() {
+    
+    let informacion ={
+        titulo: titulo.value,
+        descripcion: descripcion.value,
+        año : año.value,
+        usuario :usuario.Correo
+    };
+
+    proyectList = JSON.parse(localStorage.getItem("Proyectos")) || [];
+    proyectList.push(informacion);
+    localStorage.setItem("Proyectos", JSON.stringify(proyectList)); 
+
+
 }
 
 
@@ -106,13 +171,28 @@ let Modal2 = document.querySelector(".modal2");
 abrirModal2.addEventListener("click", ()=>{
     Modal2.showModal();
 
-    })
+})
 
 cerrarModal2.addEventListener("click", ()=>{
     Modal2.close();
     
 })
 
+// portafolio modal
+let abrirModal3 = document.getElementById("openM3");
+let cerrarModal3 = document.getElementById("closeM3");
+let Modal3 = document.querySelector(".modal3");
+
+
+abrirModal3.addEventListener("click", ()=>{
+    Modal3.showModal();
+
+    })
+
+cerrarModal3.addEventListener("click", ()=>{
+    Modal3.close();
+    
+})
 
 
 
